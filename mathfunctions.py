ea31f9ab809be7b128dc551be346056507236601
Shahnewaj Sheikh <63872577+Shahnewaj-786@users.noreply.github.com>
2022-03-02 18:38:53 +0600

    Use of Mathematical in build functions
    There is more mathematical functions for use.

---
+++ mathfunctions.py
@@ -0,0 +1,19 @@
+from math import *
+
+num1 = int (input("Enter number: "))
+num2 = float(input("Enter number: "))
+num3 = int (input("Enter number: "))
+num4 = int (input("Enter number: "))
+
+Result1 = num1+num2
+
+Result2 = num3*num4
+
+print("Maximum Result is: ",max(Result1,Result2))
+print("Maximum Result is: ",min(Result1,Result2))
+print("[2 to the power 3] is: ",pow(2,3))
+print("Square root of number3 is: ",sqrt(num3))
+
+
+
+
