bd39999acd64c51ec811f7ab94b8d1c7a6195277
Shahnewaj Sheikh <63872577+Shahnewaj-786@users.noreply.github.com>
2022-03-02 18:07:52 +0600

    typecasting in python

---
+++ typecasting.py
@@ -0,0 +1,16 @@
+print("Type Casting")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+Solution = num1+num2
+print("Result is: ",Solution)
+
+Solution = num1/num2
+print("Result is: ",Solution)
+
+Solution = num1*num2
+print("Result is: ",Solution)
+
+Solution = num1-num2
+print("Result is: ",Solution)
