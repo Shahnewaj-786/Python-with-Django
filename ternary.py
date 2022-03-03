223e952a9b49a439c1c44be01cd0b039f4057501
Shahnewaj Sheikh <63872577+Shahnewaj-786@users.noreply.github.com>
2022-03-04 00:59:51 +0600

    ternary operator

---
+++ ternary.py
@@ -0,0 +1,8 @@
+print("Ternary operator")
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+max = (num1 if num1>num2 else num2) #we use this line as ternary operator replace of if else
+
+print ("Max value is: ",max)
